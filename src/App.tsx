93884c774b7f511b5b9ae759be8604da74f90896
Katya Havriuk <0303gavkat@gmail.com>
2024-09-24 19:39:25 -0600

    finish project

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,56 @@
-import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import React, { useContext, useEffect } from 'react';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+import { TodoList } from './components/TodoList/TodoList';
+import { StateContext, DispatchContext } from './store/TodoContext';
+import { UserWarning } from './UserWarning';
+import { USER_ID } from './api/todos';
+import { getTodos } from './api/todos';
+import { ActionTypes } from './store/types';
+import { ErrorNotification } from './components/Error/ErrorNotification';
 
 export const App: React.FC = () => {
+  const { todos, error } = useContext(StateContext);
+  const dispatch = useContext(DispatchContext);
+
+  useEffect(() => {
+    if (USER_ID) {
+      getTodos()
+        .then(todosFromServer => {
+          dispatch({ type: ActionTypes.SET_TODOS, payload: todosFromServer });
+        })
+        .catch(() => {
+          dispatch({
+            type: ActionTypes.SET_ERROR,
+            payload: 'Unable to load todos',
+          });
+        });
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        dispatch({ type: ActionTypes.SET_ERROR, payload: null });
+      }, 3000);
+    }
+  }, [error, dispatch]);
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header />
+        <TodoList todos={todos} />
+        {todos.length > 0 && <Footer />}
+      </div>
+
+      <ErrorNotification />
     </div>
   );
 };
